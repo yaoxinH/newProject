67f995dba88366409541f6df32b6ab5e8976d796
yaoxin <1372883675@qq.com>
2020-11-01 10:20:10 +0800

    左边菜单

---
+++ src/components/home/layout-aside.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="layout-out">
+    <div class="title">
+      <img src="../../assets/imges/151600668521_.pic_hd.jpg"
+           alt="">
+    </div>
+    <el-menu default-active="2"
+             class="el-menu-vertical-demo"
+             @open="handleOpen"
+             @close="handleClose"
+             background-color="#545c64"
+             text-color="#fff"
+             active-text-color="#ffd04b">
+      <el-menu-item index="2">
+        <span slot="title">首页</span>
+      </el-menu-item>
+      <el-submenu>
+        <template slot="title">
+          <!-- <i class="el-icon-s-order"></i> -->
+          <span>内容管理</span>
+        </template>
+        <el-menu-item>发布文章</el-menu-item>
+        <el-menu-item>内容列表</el-menu-item>
+        <el-menu-item>评论列表</el-menu-item>
+        <el-menu-item>素材管理</el-menu-item>
+      </el-submenu>
+      <el-submenu>
+        <template slot="title">
+          <span>粉丝管理</span>
+        </template>
+        <el-menu-item>图文数据</el-menu-item>
+        <el-menu-item>文字概况</el-menu-item>
+        <el-menu-item>粉丝列表</el-menu-item>
+        <el-menu-item>粉丝画像</el-menu-item>
+      </el-submenu>
+
+      <el-menu-item index="5">
+        <span slot="title">账户信息</span>
+      </el-menu-item>
+    </el-menu>
+  </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+// <!-- // 图片背景颜色#2e2f32  导航背景色#353b4e 导航字体颜色#adafb5 导航字体激活颜色#ffd04b -->
+
+.layout-out {
+  background-color: #545c64;
+  //nnnn
+  width: 250px;
+  height: 100vh;
+  color: #adafb5;
+  .title {
+    text-align: center;
+    background-color: #2e2f32;
+    padding: 11px 0;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,59 @@
 <template>
-  <div>home</div>
+  <div>
+    <!-- 最外层容器  四个组件只能放在container中 -->
+    <el-container>
+      <el-aside>
+        <layout-aside></layout-aside>
+      </el-aside>
+      <el-container>
+        <el-header>头部</el-header>
+        <el-main>主要显示区域</el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 <script>
-export default {}
+import layoutAside from '../components/home/layout-aside.vue'
+export default {
+  components: {
+    'layout-aside': layoutAside
+  }
+}
 </script>
+<style lang="less" scoped>
+// .el-header {
+//   background-color: #b3c0d1;
+//   color: #333;
+//   text-align: center;
+//   line-height: 60px;
+// }
+
+// 图片背景颜色#2e2f32  导航背景色#353b4e 导航字体颜色#adafb5 导航字体激活颜色#ffd04b
+// .el-aside {
+//   background-color: #323745;
+//   color: #333;
+//   text-align: center;
+//   line-height: 200px;
+//   height: 100vh;
+// }
+
+// .el-main {
+//   background-color: #e9eef3;
+//   color: #333;
+//   text-align: center;
+//   line-height: 160px;
+// }
+
+// body > .el-container {
+//   margin-bottom: 40px;
+// }
+
+// .el-container:nth-child(5) .el-aside,
+// .el-container:nth-child(6) .el-aside {
+//   line-height: 260px;
+// }
+
+// .el-container:nth-child(7) .el-aside {
+//   line-height: 320px;
+// }
+</style>
